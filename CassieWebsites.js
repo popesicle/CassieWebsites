5ba1041ff27d368d96ea95389dec6b6cd5edbbe6
Tyler Neustaedter <rtneust@gmail.com>
2015-12-01 13:33:26 -0600

    adds a submit new site button to the app so you can add new sites in whenever.

--- CassieWebsites.js
+++ CassieWebsites.js
@@ -10,11 +10,21 @@ if (Meteor.isClient) {
 
   Template.body.events({
     "click .siteBlocks": function(address){
-      var address = this.address
-      location.href = address
-    }
-  })
+      var address = this.address;
+      location.href = address;
+    },
 
+    "click .submit": function(){
+     
+      var address = websiteAddress.value;
+      var name = websiteName.value;
+
+      Websites.insert({name:name, address:address});
+
+      websiteAddress.value = '';
+      websiteName.value = '';
+    }
+  });
 }
 
 if (Meteor.isServer) {
